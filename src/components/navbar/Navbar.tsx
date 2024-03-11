17e952a35e2726a77fecb5216701b1390a5e114c
Tommy Bowden <tom@moose17.dev>
2024-03-11 22:16:06 +0000

    navbar component and styling

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import Styles from "./Navbar.module.css";
 import Header from "../header/Header";
 import Button from "../button/Button";
+import Container from "../container/Container";
 
 const Navbar = () => {
   return (
-    <div className={Styles["navbar__container"]}>
-      <Header />
-      <Button text="About" />
-      <Button text="Contact" />
-      <Button text="Code" />
-    </div>
+    <Container>
+      <div className={Styles["navbar__inner"]}>
+        <Header />
+        <Button text="About" />
+        <Button text="Contact" />
+        <Button text="Code" />
+      </div>
+    </Container>
   );
 };
 
