8446ea9e406297c629abd296c513ddafaa344f8a
Tommy Bowden <tom@moose17.dev>
2024-03-11 18:42:33 +0000

    update name

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Styles from "./Header.module.css";
 
 const Header = () => {
-  return <div className={Styles["header__text"]}>{`TOM.BOWDEN`}</div>;
+  return <div className={Styles["header__text"]}>{`TOMMY.BOWDEN`}</div>;
 };
 
 export default Header;
