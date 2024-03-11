dde8e1507c0188b898085f6850faec9143a1517e
Tommy Bowden <tom@moose17.dev>
2024-03-11 23:56:40 +0000

    add margin to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,21 +6,24 @@ import Button from "../button/Button";
 
 const Footer = () => {
   return (
-    <Container>
-      <div className={Styles["footer__inner"]}>
-        <Header />
-        <Button
-          text="LinkedIn"
-          url="https://www.linkedin.com/in/tommybowden/"
-        />
-        <Button text="Instagram" url="https://www.instagram.com/tommoose_/" />
-        <Button text="GitHub" url="https://github.com/Mooose17" />
-        <p className={Styles["footer__copyright"]}>
-          Tommy Bowden 2024
-          <span className={Styles["footer__copyright--red"]}>©</span>
-        </p>
-      </div>
-    </Container>
+    <>
+      <div className={Styles["footer__outer"]}></div>
+      <Container>
+        <div className={Styles["footer__inner"]}>
+          <Header />
+          <Button
+            text="LinkedIn"
+            url="https://www.linkedin.com/in/tommybowden/"
+          />
+          <Button text="Instagram" url="https://www.instagram.com/tommoose_/" />
+          <Button text="GitHub" url="https://github.com/Mooose17" />
+          <p className={Styles["footer__copyright"]}>
+            Tommy Bowden 2024
+            <span className={Styles["footer__copyright--red"]}>©</span>
+          </p>
+        </div>
+      </Container>
+    </>
   );
 };
 
