55b3d876433e7b962e83bca6026a0b0cce021965
Tommy Bowden <tom@moose17.dev>
2024-03-11 18:27:43 +0000

    page and page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "../../styles/global.css";
+import Navbar from "@/components/navbar/Navbar";
+import Hero from "@/components/hero/Hero";
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 };
 
 export default index;
