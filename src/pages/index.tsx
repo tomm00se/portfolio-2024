6956e70d1bec17292cdf10af265bbfda5b706fe9
Tommy Bowden <tom@moose17.dev>
2024-03-11 22:16:19 +0000

    update homepage and global styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,32 @@ import React from "react";
 import "../../styles/global.css";
 import Navbar from "@/components/navbar/Navbar";
 import Hero from "@/components/hero/Hero";
+import computerPixelArt from "../../public/hero.svg";
+import beepBoopPixelArt from "../../public/beepboop.svg";
+import HeroText from "@/components/herotext/HeroText";
+import Grid from "@mui/material/Grid";
+import Container from "@/components/container/Container";
 
 const index = () => {
   return (
     <>
       <Navbar />
-      <Hero />
+      <Container>
+        <Grid container>
+          <Grid item xs={12} sm={6}>
+            <Hero image={computerPixelArt} />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <HeroText heroText="Graduate Software Engineer @ xDesign" />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <HeroText heroText="About, Contact and Code coming soon" />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Hero image={beepBoopPixelArt} />
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 };
