8ee5bf54f3639744b0857939738ddb9e615e09f3
Tommy Bowden <tom@moose17.dev>
2024-03-11 18:27:05 +0000

    header component file

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Styles from "./Header.module.css";
+
+const Header = () => {
+  return <div className={Styles["header__text"]}>{`BOWDEN.TOM`}</div>;
+};
+
+export default Header;
