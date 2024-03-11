f35a144b0e50e5e4ad07498f58512afe3ef619e5
Tommy Bowden <tom@moose17.dev>
2024-03-11 19:01:20 +0000

    update header and install sass

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import Styles from "./Header.module.css";
+import Styles from "./Header.module.scss";
 
 const Header = () => {
-  return <div className={Styles["header__text"]}>{`TOMMY.BOWDEN`}</div>;
+  return (
+    <>
+      <div className={Styles["header__text"]}>TOMMY</div>
+      <div className={Styles["header__text--red"]}>.</div>
+      <div className={Styles["header__text"]}>BOWDEN</div>
+    </>
+  );
 };
 
 export default Header;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Styles from "./Navbar.module.css";
 import Header from "../header/Header";
+import Button from "../button/Button";
 
 const Navbar = () => {
   return (
     <div className={Styles["navbar__container"]}>
       <Header />
+      <Button text="About" />
+      <Button text="Contact" />
+      <Button text="Code" />
     </div>
   );
 };
