63b08d11ee61face6e77dc0d0f2b6cb5a7880af1
Tommy Bowden <tom@moose17.dev>
2024-03-12 00:50:43 +0000

    updated header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,9 +5,8 @@ import Link from "next/link";
 const Header = () => {
   return (
     <Link className={Styles["header__text"]} href="/">
-      <span className={Styles["header__text"]}>TOMMY</span>
+      <span className={Styles["header__text"]}>TOMMOOSE</span>
       <span className={Styles["header__text--red"]}>.</span>
-      <span className={Styles["header__text"]}>BOWDEN</span>
     </Link>
   );
 };
